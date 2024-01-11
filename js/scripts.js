40c1c8f2a66756204fd787e893def763021c7911
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-10 19:01:32 -0800

    Establish function to get user input and event listener for window load

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// Get User's Inputs
+function aboutYou() {
+    age = document.getByElementId("age");
+    console.log(age);
+    bestFeature = document.getByElementId("bestFeature");
+    console.log(bestFeature);
+}
+
+
+// Show Dream Dates
+
+// Add window load Event Listener
+window.addEventListener("load", function () {
+    // Add Event Listener to Form
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+
+    })
+});
