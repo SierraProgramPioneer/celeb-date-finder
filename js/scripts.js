d5bce7b1f28f58aa4b43aae21d49526a0fb0abc8
SierraProgramPioneer <melissaparker520@gmail.com>
2024-01-10 19:06:40 -0800

    Set all content to hidden

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Get User's Inputs
 function aboutYou() {
-    age = document.getByElementId("age");
+    age = document.getByElementId("age").value;
     console.log(age);
-    bestFeature = document.getByElementId("bestFeature");
+    bestFeature = document.getByElementId("bestFeature").value;
     console.log(bestFeature);
 }
 
@@ -14,6 +14,21 @@ window.addEventListener("load", function () {
     // Add Event Listener to Form
     let form = document.querySelector("form");
     form.addEventListener("submit", function (event) {
+        // Prevent Default Refresh
+        event.preventDefault();
+
+        // Set All Content to Hidden
+        let lowe = document.querySelector("lowe")
+        lowe.setAttribute("class", "hidden");
+
+        let carell = document.querySelector("carell")
+        carell.setAttribute("class", "hidden");
+
+        let holland = document.querySelector("holland")
+        holland.setAttribute("class", "hidden");
+
+        let noDate = document.querySelector("noDate")
+        noDate.setAttribute("class", "hidden");
 
     })
 });
